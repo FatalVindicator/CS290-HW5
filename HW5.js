9beac65a7349fe866ab591add3004fc90241f154
FatalVindicator <bamarandino@gmail.com>
2020-11-06 15:17:47 -0500

    Created

---
+++ HW5.js
@@ -0,0 +1,37 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 4787);
+
+app.post('/post-page',function(req,res){
+  res.render('post-page')
+});
+
+app.get('/get-page',function(req,res){
+  res.render('get-page');
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
